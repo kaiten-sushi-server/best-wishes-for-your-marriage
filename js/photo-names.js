e4732d5089648043b9f3b5087f91711411424194
sukka65536 <sukka65536.2@gmail.com>
2026-06-21 20:32:52 +0900

    pre 0.3

--- js/photo-names.js
+++ js/photo-names.js
@@ -2,11 +2,11 @@
 
 const photos = {
     individual: [
-        'sukka.png', 'temp.png', 'temp.png', 'temp.png',
-        'temp.png', 'temp.png', 'temp.png', 'temp.png',
-        'temp.png', 'temp.png', 'temp.png', 'temp.png',
-        'temp.png', 'temp.png', 'temp.png', 'temp.png',
-        'temp.png', 'temp.png', 'temp.png', 'temp.png'
+        'asahosidonki.png', 'ba_bbaba888.png', 'hazime_3333.png', 'KOOMA05.png',
+        'koto_bukimo_sub.png', 'Kurosu_kana.png', 'midorino_omochi.png', 'Mikanha_315.png',
+        'miku_3901_vrc.png', 'MinemineVR.jpg', 'Mr_donut029.png', 'nekotamo0234.png',
+        'OMEGA11_VRC.png', 'pachikoma_vr.png', 'sora_chamo.jpg', 'sukka65536.png',
+        'tanaca_8.png', 'tugu5757.jpg', 'vryoituki4183.png', 'Yuri3Haruku3.png'
     ],
-    group: ['temp.png']
+    group: ['_group.png']
 }
